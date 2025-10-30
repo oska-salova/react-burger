0a5154ec6a9772e4b45fa7291ac08af9a80b5108
Olga <oska.salova@yandex.ru>
2025-10-30 23:01:50 +0700

    edit user page layout

--- src/pages/user/profile.tsx
+++ src/pages/user/profile.tsx
@@ -1,23 +1,43 @@
-import { NavLink, Outlet } from 'react-router-dom';
+import { NavLink, Outlet, useHref, useLocation } from 'react-router-dom';
 import { ProfileRoutes } from '../config';
+import styles from './profile.module.css';
 
 function ProfilePage() {
+	const location = useLocation();
+	const userHref = useHref(ProfileRoutes.User, { relative: 'path' });
+
+	const description =
+		location.pathname === userHref
+			? 'В этом разделе вы можете изменить свои персональные данные'
+			: 'В этом разделе вы можете просмотреть свою историю заказов';
 	return (
-		<section className="flex-center">
-			<nav>
+		<section className={styles.page}>
+			<nav className={styles.menu}>
 				<NavLink
 					to={ProfileRoutes.User}
 					end
-					className={({ isActive }) => [isActive ? 'active' : ''].join(' ')}
+					className={({ isActive }) =>
+						[isActive ? 'active' : '', styles.menuItem].join(' ')
+					}
 				>
 					<p className="text text_type_main-default">Профиль</p>
 				</NavLink>
-				<NavLink to={ProfileRoutes.Orders}>
+				<NavLink to={ProfileRoutes.Orders} className={styles.menuItem}>
 					<p className="text text_type_main-default">История заказов</p>
 				</NavLink>
-				<p className="text text_type_main-default text_color_inactive">Выход</p>
+				<p className={`text text_type_main-default text_color_inactive ${styles.menuItem}`}>
+					Выход
+				</p>
+				<p
+					className={`text text_type_main-default text_color_inactive mt-20 ${styles.description}`}
+				>
+					{description}
+				</p>
 			</nav>
-			<Outlet />
+
+			<section className={styles.content}>
+				<Outlet />
+			</section>
 		</section>
 	);
 }
--- src/pages/user/user.tsx
+++ src/pages/user/user.tsx
@@ -1,7 +1,92 @@
+import {
+	Button,
+	EmailInput,
+	Input,
+	PasswordInput,
+} from '@ya.praktikum/react-developer-burger-ui-components';
+import { useAppDispatch, useAppSelector } from '../../services/store';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import styles from './user.module.css';
+
 function UserPage() {
+	const dispatch = useAppDispatch();
+	const userState = useAppSelector(state => state.userReducer);
+	const user = userState.user;
+
+	const [form, setFormValue] = useState({
+		name: user?.name ?? '',
+		email: user?.email ?? '',
+		password: '',
+	});
+	const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setFormValue({ ...form, [e.target.name]: e.target.value });
+	};
+
+	const isUserDataModified =
+		form.name !== (user?.name ?? '') || form.email !== (user?.email ?? '') || !!form.password;
+
+	const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		console.log('** handle submit ', form);
+		// dispatch(registerUser(form));
+	};
+
+	const isSubmitAvailable = Object.values(form).every(value => !!value) && !userState.pending;
+
 	return (
-		<section className="flex-center">
-			<p className="text text_type_main-default">User page</p>
+		<section className={`flex-center ${styles.page}`}>
+			<form className={styles.form} onSubmit={handleSubmit}>
+				<Input
+					type="text"
+					placeholder="Имя"
+					onChange={onChange}
+					value={form.name}
+					name="name"
+					extraClass="mb-6"
+					icon="EditIcon"
+					onPointerEnterCapture={undefined}
+					onPointerLeaveCapture={undefined}
+				/>
+				<EmailInput
+					onChange={onChange}
+					value={form.email}
+					name="email"
+					isIcon={true}
+					extraClass="mb-6"
+				/>
+				<PasswordInput
+					onChange={onChange}
+					value={form.password}
+					name="password"
+					icon="EditIcon"
+					extraClass="mb-6"
+				/>
+				{userState.error && (
+					<p className="text text_type_main-default text_color_error mb-6">
+						{userState.error}
+					</p>
+				)}
+				{isUserDataModified && (
+					<footer className={styles.footer}>
+						<Button
+							htmlType="reset"
+							type="secondary"
+							size="medium"
+							disabled={!isSubmitAvailable}
+						>
+							Отмена
+						</Button>
+						<Button
+							htmlType="submit"
+							type="primary"
+							size="medium"
+							disabled={!isSubmitAvailable}
+						>
+							Сохранить
+						</Button>
+					</footer>
+				)}
+			</form>
 		</section>
 	);
 }
