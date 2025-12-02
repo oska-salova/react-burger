6025bc691b8c0679eb5dcc43dcffa86731bdccbb
Olga <oska.salova@yandex.ru>
2025-12-02 21:37:05 +0700

    history layout

--- src/pages/order/order-history.tsx
+++ src/pages/order/order-history.tsx
@@ -1,12 +1,10 @@
 import OrderCards from '../../components/order/order-cards/order-cards';
+import styles from './order-history.module.css';
 
 function OrderHistoryPage() {
 	return (
-		<section className="flex-center">
-			{/* <p className="text text_type_main-default">Order history page</p> */}
-			<div className="">
-				<OrderCards isHistory={true} />
-			</div>
+		<section className={`flex-center ${styles.history}`}>
+			<OrderCards isHistory={true} />
 		</section>
 	);
 }
