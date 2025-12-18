81a2bb71c1c1cb960999322d9f6420149bf97d73
Olga <oska.salova@yandex.ru>
2025-12-19 01:29:41 +0700

    review fixes (cypress)

--- cypress/e2e/burger-constructor/burger-constructor.spec.cy.ts
+++ cypress/e2e/burger-constructor/burger-constructor.spec.cy.ts
@@ -1,68 +1,69 @@
 import { mockGetIngredients } from '../../mocks/mocks';
 
 describe('burger constructor', function () {
+	const testSingleBunId = '643d69a5c3f7b9001cfa093c';
+	const testSingleBunName = 'Краторная булка N-200i';
+
 	beforeEach(() => {
 		cy.visit('/');
 		mockGetIngredients();
 		cy.wait('@ingredients');
+		cy.get('[data-testid="constructor-bun-top"]').as('constructorTopBun');
+		cy.get('[data-testid="constructor-bun-bottom"]').as('constructorBottomBun');
 	});
 
 	it('should add 2 buns to burger when bun drops to burger bun placeholder', () => {
-		cy.get('[data-testid="constructor-bun-top"]').find('img').should('not.exist');
-		cy.get('[data-testid="constructor-bun-bottom"]').find('img').should('not.exist');
-		cy.get('[data-testid="constructor-bun-top"]').contains('Выберите булку').should('exist');
-		cy.get('[data-testid="constructor-bun-bottom"]').contains('Выберите булку').should('exist');
+		cy.get('@constructorTopBun').contains('span', testSingleBunName).should('not.exist');
+		cy.get('@constructorBottomBun').contains('span', testSingleBunName).should('not.exist');
+		cy.get('@constructorTopBun').contains('Выберите булку').should('exist');
+		cy.get('@constructorBottomBun').contains('Выберите булку').should('exist');
 
-		cy.get('[data-testid="ingredient-643d69a5c3f7b9001cfa093c"]').trigger('dragstart');
-		cy.get('[data-testid="constructor-bun-top"]').trigger('drop');
+		cy.get(`[data-testid="ingredient-${testSingleBunId}"]`).dragTo('@constructorTopBun');
 
-		cy.get('[data-testid="constructor-bun-top"]').find('img').should('exist');
-		cy.get('[data-testid="constructor-bun-bottom"]').find('img').should('exist');
-		cy.get('[data-testid="constructor-bun-top"]')
-			.contains('Выберите булку')
-			.should('not.exist');
-		cy.get('[data-testid="constructor-bun-bottom"]')
-			.contains('Выберите булку')
-			.should('not.exist');
+		cy.get('@constructorTopBun').contains('span', testSingleBunName).should('exist');
+		cy.get('@constructorBottomBun').contains('span', testSingleBunName).should('exist');
+		cy.get('@constructorTopBun').contains('Выберите булку').should('not.exist');
+		cy.get('@constructorBottomBun').contains('Выберите булку').should('not.exist');
 	});
 
 	it('should not add fillings when drops to burger bun placeholder', () => {
-		cy.get('[data-testid="constructor-bun-top"]').find('img').should('not.exist');
-		cy.get('[data-testid="constructor-bun-bottom"]').find('img').should('not.exist');
-		cy.get('[data-testid="constructor-bun-top"]').contains('Выберите булку').should('exist');
-		cy.get('[data-testid="constructor-bun-bottom"]').contains('Выберите булку').should('exist');
+		cy.get('@constructorTopBun').contains('span', testSingleBunName).should('not.exist');
+		cy.get('@constructorBottomBun').contains('span', testSingleBunName).should('not.exist');
+		cy.get('@constructorTopBun').contains('Выберите булку').should('exist');
+		cy.get('@constructorBottomBun').contains('Выберите булку').should('exist');
 
-		cy.get('[data-testid="ingredient-643d69a5c3f7b9001cfa093e"]').trigger('dragstart');
-		cy.get('[data-testid="constructor-bun-top"]').trigger('drop');
+		cy.get('[data-testid="ingredient-643d69a5c3f7b9001cfa093e"]').dragTo('@constructorTopBun');
 
-		cy.get('[data-testid="constructor-bun-top"]').find('img').should('not.exist');
-		cy.get('[data-testid="constructor-bun-bottom"]').find('img').should('not.exist');
-		cy.get('[data-testid="constructor-bun-top"]').contains('Выберите булку').should('exist');
-		cy.get('[data-testid="constructor-bun-bottom"]').contains('Выберите булку').should('exist');
+		cy.get('@constructorTopBun').contains('span', testSingleBunName).should('not.exist');
+		cy.get('@constructorBottomBun').contains('span', testSingleBunName).should('not.exist');
+		cy.get('@constructorTopBun').contains('Выберите булку').should('exist');
+		cy.get('@constructorBottomBun').contains('Выберите булку').should('exist');
 	});
 
 	it('should add filling when drops to burger filling placeholder', () => {
 		cy.get('[data-testid="constructor-filling-item-empty-filling"]').should('exist');
 
-		cy.get('[data-testid="ingredient-643d69a5c3f7b9001cfa093e"]').trigger('dragstart');
-		cy.get('[data-testid="constructor-filling-ul"]').trigger('drop');
+		cy.get('[data-testid="ingredient-643d69a5c3f7b9001cfa093e"]').dragTo(
+			'[data-testid="constructor-filling-ul"]',
+		);
 
 		cy.get('[data-testid="constructor-filling-item-empty-filling"]').should('not.exist');
 	});
 
 	it('should not add bun when drops to burger filling placeholder', () => {
-		cy.get('[data-testid="constructor-bun-top"]').find('img').should('not.exist');
-		cy.get('[data-testid="constructor-bun-bottom"]').find('img').should('not.exist');
-		cy.get('[data-testid="constructor-bun-top"]').contains('Выберите булку').should('exist');
-		cy.get('[data-testid="constructor-bun-bottom"]').contains('Выберите булку').should('exist');
+		cy.get('@constructorTopBun').contains('span', testSingleBunName).should('not.exist');
+		cy.get('@constructorBottomBun').contains('span', testSingleBunName).should('not.exist');
+		cy.get('@constructorTopBun').contains('Выберите булку').should('exist');
+		cy.get('@constructorBottomBun').contains('Выберите булку').should('exist');
 
-		cy.get('[data-testid="ingredient-643d69a5c3f7b9001cfa093c"]').trigger('dragstart');
-		cy.get('[data-testid="constructor-filling-ul"]').trigger('drop');
+		cy.get('[data-testid="ingredient-643d69a5c3f7b9001cfa093c"]').dragTo(
+			'[data-testid="constructor-filling-ul"]',
+		);
 
-		cy.get('[data-testid="constructor-bun-top"]').find('img').should('not.exist');
-		cy.get('[data-testid="constructor-bun-bottom"]').find('img').should('not.exist');
-		cy.get('[data-testid="constructor-bun-top"]').contains('Выберите булку').should('exist');
-		cy.get('[data-testid="constructor-bun-bottom"]').contains('Выберите булку').should('exist');
+		cy.get('@constructorTopBun').contains('span', testSingleBunName).should('not.exist');
+		cy.get('@constructorBottomBun').contains('span', testSingleBunName).should('not.exist');
+		cy.get('@constructorTopBun').contains('Выберите булку').should('exist');
+		cy.get('@constructorBottomBun').contains('Выберите булку').should('exist');
 	});
 
 	it('should add all properly dropped same fillings', () => {
@@ -72,8 +73,9 @@ describe('burger constructor', function () {
 		const dropsCount = 5;
 
 		for (let i = 0; i < dropsCount; i++) {
-			cy.get('[data-testid="ingredient-643d69a5c3f7b9001cfa093e"]').trigger('dragstart');
-			cy.get('[data-testid="constructor-filling-ul"]').trigger('drop');
+			cy.get('[data-testid="ingredient-643d69a5c3f7b9001cfa093e"]').dragTo(
+				'[data-testid="constructor-filling-ul"]',
+			);
 		}
 
 		cy.get('[data-testid="constructor-filling-item-empty-filling"]').should('not.exist');
@@ -92,8 +94,9 @@ describe('burger constructor', function () {
 		];
 
 		for (let i = 0; i < fillings.length; i++) {
-			cy.get(`[data-testid="ingredient-${fillings[i]}"]`).trigger('dragstart');
-			cy.get('[data-testid="constructor-filling-ul"]').trigger('drop');
+			cy.get(`[data-testid="ingredient-${fillings[i]}"]`).dragTo(
+				'[data-testid="constructor-filling-ul"]',
+			);
 		}
 
 		cy.get('[data-testid="constructor-filling-item-empty-filling"]').should('not.exist');
--- cypress/e2e/order/register-order.spec.cy.ts
+++ cypress/e2e/order/register-order.spec.cy.ts
@@ -11,29 +11,32 @@ describe('register order', function () {
 
 	it('should add order create button when bun added', () => {
 		cy.get('[data-testid="register-order-btn"]').should('not.exist');
-		cy.get('[data-testid="ingredient-643d69a5c3f7b9001cfa093c"]').trigger('dragstart');
-		cy.get('[data-testid="constructor-bun-top"]').trigger('drop');
+		cy.get('[data-testid="ingredient-643d69a5c3f7b9001cfa093c"]').dragTo(
+			'[data-testid="constructor-bun-top"]',
+		);
 		cy.get('[data-testid="register-order-btn"]').should('exist');
 	});
 
 	it('should disable order create button while order register request is in progress', () => {
 		mockRegisterOrder();
 
-		cy.get('[data-testid="ingredient-643d69a5c3f7b9001cfa093c"]').trigger('dragstart');
-		cy.get('[data-testid="constructor-bun-top"]').trigger('drop');
-		cy.get('[data-testid="register-order-btn"]').should('be.enabled');
-		cy.get('[data-testid="register-order-btn"]').click();
+		cy.get('[data-testid="ingredient-643d69a5c3f7b9001cfa093c"]').dragTo(
+			'[data-testid="constructor-bun-top"]',
+		);
+		cy.get('[data-testid="register-order-btn"]').as('orderBtn').should('be.enabled');
+		cy.get('@orderBtn').click();
 		cy.wait('@user');
-		cy.get('[data-testid="register-order-btn"]').should('be.disabled');
+		cy.get('@orderBtn').should('be.disabled');
 		cy.wait('@registerOrder');
-		cy.get('[data-testid="register-order-btn"]').should('not.exist');
+		cy.get('@orderBtn').should('not.exist');
 	});
 
 	it('should open order details modal when order register request succeeded', () => {
 		mockRegisterOrder();
 
-		cy.get('[data-testid="ingredient-643d69a5c3f7b9001cfa093c"]').trigger('dragstart');
-		cy.get('[data-testid="constructor-bun-top"]').trigger('drop');
+		cy.get('[data-testid="ingredient-643d69a5c3f7b9001cfa093c"]').dragTo(
+			'[data-testid="constructor-bun-top"]',
+		);
 		cy.get('[data-testid="register-order-btn"]').click();
 		cy.wait('@user');
 		cy.get('[data-testid="order-details"]').should('not.exist');
